57e6a352899d0fc005da0841255ed0ef733d0c6f
Greg Kubok <gkubok@gmail.com>
2019-12-02 13:35:01 +0000

    Adding vault to Brew packages.

--- macos/brew.sh
+++ macos/brew.sh
@@ -8,7 +8,7 @@ xcode-select --install
 brew analytics off
 
 # Install Brew casks
-brew cask install 1password bettertouchtool caffeine calibre docker drawio dropbox firefox google-chrome google-cloud-sdk google-photos-backup-and-sync iterm2 java karabiner-elements keybase lastpass osxfuse skype slack smoothscroll spotify tigervnc-viewer tunnelblick vagrant virtualbox visual-studio-code vlc xquartz
+brew cask install 1password bettertouchtool caffeine calibre docker drawio dropbox firefox google-chrome google-cloud-sdk google-photos-backup-and-sync iterm2 java karabiner-elements keybase lastpass osxfuse skype slack smoothscroll spotify tigervnc-viewer tunnelblick vagrant vault virtualbox visual-studio-code vlc xquartz
 
 # Install Brew packages
 brew install ansible autojump awscli coreutils curl dep direnv docker docker-completion docker-compose dos2unix azure/draft/draft exiftool ffmpeg flac fontconfig freetype fzf gettext git gnupg go graphviz helm htop imagemagick irssi jq derailed/k9s/k9s kubernetes-cli kubernetes-helm lua midnight-commander minikube mkcert mplayer mpv neovim nmap openssl packer perl pwgen python yq ruby versent/homebrew-taps/saml2aws screen scrub skaffold sqlite sshfs telnet terraform terragrunt tmux tree unrar weechat wget xz zsh zsh-autosuggestions
