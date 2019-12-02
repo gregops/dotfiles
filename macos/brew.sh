a0cb306d576bb4eb3c089d600b2025d79ade4d8c
Greg Kubok <gkubok@gmail.com>
2019-12-02 09:24:50 +0000

    Adding MacOS setup scripts.

---
+++ macos/brew.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install command line tools
+xcode-select --install
+
+# Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew analytics off
+
+# Install Brew casks
+brew cask install 1password bettertouchtool caffeine calibre docker drawio dropbox firefox google-chrome google-cloud-sdk google-photos-backup-and-sync iterm2 java karabiner-elements keybase lastpass osxfuse skype slack smoothscroll spotify tigervnc-viewer tunnelblick vagrant virtualbox visual-studio-code vlc xquartz
+
+# Install Brew packages
+brew install ansible autojump awscli coreutils curl dep direnv docker docker-completion docker-compose dos2unix azure/draft/draft exiftool ffmpeg flac fontconfig freetype fzf gettext git gnupg go graphviz helm htop imagemagick irssi jq derailed/k9s/k9s kubernetes-cli kubernetes-helm lua midnight-commander minikube mkcert mplayer mpv neovim nmap openssl packer perl pwgen python yq ruby versent/homebrew-taps/saml2aws screen scrub skaffold sqlite sshfs telnet terraform terragrunt tmux tree unrar weechat wget xz zsh zsh-autosuggestions
---
+++ macos/fonts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install fonts
+for font in $(brew search nerd-font | cat | cut -f3 -d/ | grep ^font | grep -v -- -mono$); do
+  brew cask install homebrew/cask-fonts/${font}
+done
+cd
+git clone https://github.com/2players/sfmono-nerd-font
+cp ~/sfmono-nerd-font/*.otf ~/Library/Fonts/
+rm -rf sfmono-nerd-font
+
